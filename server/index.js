d76695db1ceb6511f3a14024226eb9c57355922e
Edison Wang <edisonwang03@gmail.com>
2023-05-13 11:45:43 -0400

    Connect via node-postgres

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const pool = require("./db");
-
-//middleware
-app.use(cors());
-app.use(express.json()); //req.body
 
 //ROUTES//
 
